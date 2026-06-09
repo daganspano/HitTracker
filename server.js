a01ef81e13b0924996a70abcc2518d4f6f2325ac
Dagan Spano <daganspano@gmail.com>
2026-06-09 14:28:43 -0500

    Deployment fixes

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(join(__dirname, "dist")));
+
+// Handle SPA routing - serve index.html for all non-file routes
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
